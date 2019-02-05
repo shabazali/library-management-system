3f936241ef8b70101c3d36292e04633bfa8c1eed
shabaz <nayabshabazali@gmail.com>
2019-02-05 13:25:26 +0530

    use the net beans for this project
    The .form are the frontend GUI created to interact and .java files are the codes for every form named similarly. Some pages you need to add library packages based on errors. you can google the errors and find the solutions. the other .png files are simply added for icons in the buttons etc. you can neglect them and use your own icons. all the best

---
+++ rs.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author K.CHINNA
+ */
+class rs {
+
+    static boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
